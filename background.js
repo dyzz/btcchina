53507c5f334d1a3e977f31371b66009a3cd20063
Zhong Zhuang <zhong.zhuang@yale.edu>
2013-03-07 12:11:36 +0800

    1st version
    TODO: use canvas to generate img and setActionIcon to display nice price
    for both rmb and usd

--- background.js
+++ background.js
@@ -1,57 +1,52 @@
 // 1 min interval
-var pollInterval = 1;
+var pollInterval = 5*60*1000;
 
 var url = "https://btcchina.com/";
 
+// function scheduleRequest() {
+//   console.log('start query');
+//   var delay = pollInterval;
+//   console.log('creating alarm');
+//   chrome.alarms.create('refresh', {periodInMinutes: delay});
+// }
 
-function updateBadge(x,y) {
-  if(x>0 && y>0)
-    chrome.browserAction.setBadgeText({text:cur_rmb+"/"+cur_usd});
-  else
-    chrome.browserAction.setBadgeText({text:"?"});
-};
-
-function scheduleRequest() {
-  console.log('start query');
-  var delay = pollInterval;
-  console.log('creating alarm');
-  chrome.alarms.create('refresh', {periodInMinutes: delay});
-}
-
-function startRequest() {
-   scheduleRequest();
-  getCurrentPrice();
-}
+// function startRequest() {
+//    scheduleRequest();
+//   getCurrentPrice();
+// }
 
 
 
-function getCurrentPrice() {
-  alert("get price");
+function update() {
   $.ajax({    
 	   url:url,
 	   success:function(data){
-	     var cur_rmb = $('.infobox:nth(0)  tr:nth(1) td:nth(1)',$(data)).html().slice(1);
-	     var cur_usd = $('.infobox:nth(1)  tr:nth(1) td:nth(1)',$(data)).html().slice(1);
-	     updateBadge(cur_rmb,cur_usd);
+	       var cur_rmb = $('.infobox:nth(0)  tr:nth(1) td:nth(1)',$(data)).html().slice(1);
+	       var cur_usd = $('.infobox:nth(1)  tr:nth(1) td:nth(1)',$(data)).html().slice(1);
+	       // chrome.browserAction.setBadgeText({'text':cur_rmb+"/"+cur_usd});
+	       chrome.browserAction.setBadgeText({'text':parseInt(cur_rmb)+""});
 	   },
 	   error:function(err){
-	     updateBadge(-1,-1);
+	       chrome.browserAction.setBadgeText({'text':"?"});
 	   }
 	 }
 	);
 }
 
-
-function onInit() {
-  console.log('onInit');
-  startRequest();
-}
-
-function onAlarm(alarm) {
-  startRequest();
-}
+setInterval(update,pollInterval);
+update();
 
 
+// function onInit() {
+//   console.log('onInit');
+//   startRequest();
+// }
 
-chrome.runtime.onInstalled.addListener(onInit);
-chrome.alarms.onAlarm.addListener(onAlarm);
+// function onAlarm(alarm) {
+//   startRequest();
+// }
+
+
+
+// chrome.runtime.onInstalled.addListener(onInit);
+// chrome.alarms.onAlarm.addListener(onAlarm);
