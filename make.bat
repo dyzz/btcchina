539fa3a190eb34d4a6109d255b37ed60f4e7409f
Zhong Zhuang <naituida@foxmail.com>
2013-03-26 19:28:57 +0800

    uft-8 fix

--- make.bat
+++ make.bat
@@ -1,2 +1,2 @@
 rm *.*~
-zip btcchina.zip -u icon.png  manifest.json jquery-1.9.1.min.js animator.js background.html background.js option.html option.js popup.html
+zip btcchina.zip -u icon.png small-icon.png manifest.json jquery-1.9.1.min.js animator.js background.html background.js option.html option.js popup.html
